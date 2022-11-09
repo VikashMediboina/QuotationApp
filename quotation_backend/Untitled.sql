61a95f374a5507c2ff5e1efa1422473a33350899
vikash mediboina <vikashmediboina@vikashs-MacBook-Pro.local>
2022-11-08 19:42:43 -0500

    changes are done

--- quotation_backend/Untitled.sql
+++ quotation_backend/Untitled.sql
@@ -12,8 +12,6 @@ CREATE DATABASE "QuotationDB"
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
